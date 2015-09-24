e6bd9a8224593dbb05c2eb30553c6fb7b7d8504f
冷子欲 <1058566903@qq.com>
2015-09-24 14:25:44 +0800

    jquery.tab-0.0.1版
    添加了可自动切换功能，支持点击和经过事件，更多自定义参数，样式方面

--- jquery.tab.js
+++ jquery.tab.js
@@ -1,21 +1,77 @@
+/*
+* jquery.tab 0.0.1
+* Copyright (c) 2015 lengziyu http://lengziyu.com/
+* Date: 2015-09-24
+* 使用jquery.tab方便地使用切换功能，支持点击和经过事件，自动切换和手动切换，还有一些可自定义参数
+*/
 ;(function($){
   $.fn.tab = function(options){
     var defaults = {
-      myTab:".tab",
-      myTabMenu:".menu",
-      myTabMain:".main"
+      //默认按钮菜单
+      TabMenu:".menu",
+      //默认切换
+      TabMain:".main",
+      //当前项
+      TabActive:"active",
+      //默认自动切换时间
+      Time:"1000",
+      //默认点击切换事件，mouseenter可以设置鼠标划过事件
+      Event:"click",
+      //是否自动播放
+      Auto:true
+
     };
+
     var options = $.extend(defaults, options);
+
     this.each(function(){
       var thisTab = $(this);
-      var thisTabMenu = $(thisTab).find(defaults.myTabMenu);
-      var thisTabMain = $(thisTab).find(defaults.myTabMain);
-      thisTabMenu.on("click","li",function(){
-        var tihsCurrent = $(this);
-        $(this).addClass("current").siblings().removeClass("current");
-        thisTabMain.find("li").eq(thisTabMenu.children().index(tihsCurrent)).addClass("current").siblings().removeClass("current");
+      var thisTabMenu = $(thisTab).find(options.TabMenu);
+      var thisTabMain = $(thisTab).find(options.TabMain);
+
+      var i = 0;//初始记录用户鼠标经过是第几个li
+      var canmove = true;
+      thisTabMenu.on(options.Event,"li",function(){
+        canmove = false;
+        clearInterval(li_time);
+        i = $(this).index();
+        //设置当前类名
+        $(this).addClass(options.TabActive).siblings().removeClass(options.TabActive);
+        thisTabMain.find("li").eq(i).addClass(options.TabActive).show().siblings().removeClass(options.TabActive).hide();
         return false
       });
+
+      //鼠标放上去则停止自动播放，否则自动播放
+      thisTab.mouseenter(function(){
+        canmove = false;
+      }).mouseleave(function(){
+        clearInterval(li_time);
+
+        //一秒后自动切换
+        setTimeout(function(){
+          canmove = true;
+        },options.Time);
+      });
+
+      function li_time(){
+        if(canmove){
+          i++;
+          //如果是最后一个，则跳回第一个
+          var len = thisTabMenu.find("li").length;
+          if(i == len){
+            i = 0;
+          }
+          thisTabMenu.find("li").eq(i).addClass(options.TabActive).siblings().removeClass(options.TabActive);
+          thisTabMain.find("li").eq(i).addClass(options.TabActive).show().siblings().removeClass(options.TabActive).hide();
+        }
+      }
+      //如果Auto是true则自动播放（setInterval），否则（clearInterval）
+      if(options.Auto == true){
+        Autois = setInterval;
+      }else{
+        Autois = clearInterval;
+      }
+      Autois(li_time,options.Time);
     });
   };
 })(jQuery);
