39a1e7ad81c8e94e163ad9913542107608ba3e21
冷子欲 <1058566903@qq.com>
2015-10-10 12:22:01 +0800

    v0.0.2版
    加入懒加载、优化、修复BUG

--- jquery.tab.js
+++ jquery.tab.js
@@ -1,77 +1,90 @@
 /*
-* jquery.tab 0.0.1
+* jquery.tab 0.0.2
 * Copyright (c) 2015 lengziyu http://lengziyu.com/
 * Date: 2015-09-24
 * 使用jquery.tab方便地使用切换功能，支持点击和经过事件，自动切换和手动切换，还有一些可自定义参数
 */
 ;(function($){
-  $.fn.tab = function(options){
+  $.fn.tab = function(opts){
     var defaults = {
       //默认按钮菜单
-      TabMenu:".menu",
+      menu:".menu",
       //默认切换
-      TabMain:".main",
+      main:".main",
       //当前项
-      TabActive:"active",
+      active:"active",
       //默认自动切换时间
-      Time:"1000",
+      delay:"1000",
       //默认点击切换事件，mouseenter可以设置鼠标划过事件
-      Event:"click",
+      event:"click",
       //是否自动播放
-      Auto:true
+      auto:true,
+      //懒加载
+      lazyload:true
 
     };
 
-    var options = $.extend(defaults, options);
+    var opts = $.extend(defaults, opts);
+
 
     this.each(function(){
-      var thisTab = $(this);
-      var thisTabMenu = $(thisTab).find(options.TabMenu);
-      var thisTabMain = $(thisTab).find(options.TabMain);
+      //获取元素
+      var t = $(this),
+            menu = t.find(opts.menu),
+            main = t.find(opts.main),
+            ec = menu.children(),
+            ac = main.children(),
+            i = 0,
+            canmove = true;
 
-      var i = 0;//初始记录用户鼠标经过是第几个li
-      var canmove = true;
-      thisTabMenu.on(options.Event,"li",function(){
+      //懒加载
+      function lazy(){
+          if(!opts.lazyload == false){
+            var src = ac.eq(i).find('img.lazy').attr("data-src");
+            ac.eq(i).find('img.lazy').attr("src",src).removeAttr("data-src");
+          }
+      }
+
+      //点击切换
+      menu.on(opts.event,"li",function(){
         canmove = false;
-        clearInterval(li_time);
+        clearInterval(li_delay);
         i = $(this).index();
         //设置当前类名
-        $(this).addClass(options.TabActive).siblings().removeClass(options.TabActive);
-        thisTabMain.find("li").eq(i).addClass(options.TabActive).show().siblings().removeClass(options.TabActive).hide();
+        $(this).addClass(opts.active).siblings().removeClass(opts.active);
+        ac.eq(i).show().addClass(opts.active).siblings().removeClass(opts.active).hide();
+        lazy();
         return false
       });
 
       //鼠标放上去则停止自动播放，否则自动播放
-      thisTab.mouseenter(function(){
+      t.mouseenter(function(){
         canmove = false;
       }).mouseleave(function(){
-        clearInterval(li_time);
-
-        //一秒后自动切换
-        setTimeout(function(){
           canmove = true;
-        },options.Time);
       });
 
-      function li_time(){
+      //多少时间后切换函数
+      function li_delay(){
         if(canmove){
           i++;
           //如果是最后一个，则跳回第一个
-          var len = thisTabMenu.find("li").length;
+          var len = menu.children().length;
           if(i == len){
             i = 0;
           }
-          thisTabMenu.find("li").eq(i).addClass(options.TabActive).siblings().removeClass(options.TabActive);
-          thisTabMain.find("li").eq(i).addClass(options.TabActive).show().siblings().removeClass(options.TabActive).hide();
+          ec.eq(i).addClass(opts.active).siblings().removeClass(opts.active);
+          ac.eq(i).show().addClass(opts.active).siblings().removeClass(opts.active).hide();
+          lazy();
         }
       }
-      //如果Auto是true则自动播放（setInterval），否则（clearInterval）
-      if(options.Auto == true){
-        Autois = setInterval;
+
+      //如果auto是true则自动播放（setInterval），否则（clearInterval）
+      if(opts.auto == true){
+        setInterval(li_delay,opts.delay);
       }else{
-        Autois = clearInterval;
+        clearInterval(li_delay,opts.delay);
       }
-      Autois(li_time,options.Time);
     });
   };
 })(jQuery);
